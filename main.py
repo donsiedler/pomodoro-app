11344449be61060797d5141ebb7116c8b8eb6d91
Artur Siedlerski <don.siedler@gmail.com>
2023-01-14 21:31:58 +0100

    fix seconds format

--- main.py
+++ main.py
@@ -16,13 +16,16 @@ CHECK_MARK = "✔"
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
-    count_down(25 * 60)
+    count_down(12)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
     count_min = count // 60
     count_sec = count % 60
+
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
@@ -40,7 +43,7 @@ app_label.grid(row=0, column=1)
 canvas = tkinter.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = tkinter.PhotoImage(file="images/tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-timer_text = canvas.create_text(100, 138, text="25:00", fill="white", font=(FONT_NAME, 28, "bold"))
+timer_text = canvas.create_text(100, 138, text="00:00", fill="white", font=(FONT_NAME, 28, "bold"))
 canvas.grid(row=1, column=1)
 
 start_btn = tkinter.Button(text="Start", highlightthickness=0, command=start_timer)
