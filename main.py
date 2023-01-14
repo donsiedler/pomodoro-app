0a5e28b28bb2aa49e042b8b3d47de39d079df438
Artur Siedlerski <don.siedler@gmail.com>
2023-01-14 22:02:15 +0100

    add timer logic

--- main.py
+++ main.py
@@ -10,16 +10,29 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 CHECK_MARK = "✔"
+reps = 0
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
-    count_down(12)
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+    elif reps % 2 == 1:
+        count_down(work_sec)
+    else:
+        count_down(short_break_sec)
 
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
     count_min = count // 60
     count_sec = count % 60
@@ -30,6 +43,8 @@ def count_down(count):
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
+    else:  # Count goes to 0
+        start_timer()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
